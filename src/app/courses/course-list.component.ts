f5562878b5d0b672770127ee5a4225d4d8390020
Carlos Silva <andersoncarlos158@hotmail.com>
2021-08-06 22:44:37 -0400

    Injeção de Dependências no course.service

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     selector: 'app-course-list',
@@ -10,29 +11,12 @@ export class CourseListComponent implements OnInit  {
 
     courses: Course[] = [];
 
+    constructor(private courseService: CourseService) {
+        this.courses = this.courseService.retrieveAll();
+    }
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XP-8967',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'January, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'SP-1004',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'December, 2, 2020'
-            }
-        ]
+
     }
 
 }
