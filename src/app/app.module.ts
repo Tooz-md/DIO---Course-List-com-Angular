c76fede5dbff0ee28414867130948658f61ccd89
Carlos Silva <andersoncarlos158@hotmail.com>
2021-08-10 22:36:19 -0400

    Segregação de Módulos de alguns componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './navbar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/navbar/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full' //Angular fornece duas rotas padrão: '', onde se encontra a rota raiz, ou '**', onde redireciona para uma rota que não existe.
-      },
-      {
-        path: '**', component: Error404Component
-      },
+      }
     ])
   ],
   providers: [],
